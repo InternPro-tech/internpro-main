55bcb47b624b565b8d1c667825680ae2307d8d06
InternPro-tech <internpro038@gmail.com>
2024-08-18 16:17:31 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,14 +7,14 @@ import Image from "next/image";
 export const Hero = () => {
   return (
 <div className="w-full">
-    <div className="relative flex w-full items-center justify-center overflow-hidden md:p-20 h-screen">
+    <div className="relative flex my-8 w-full items-center justify-center overflow-hidden md:p-20 h-screen">
         <div className="flex flex-col items-center">
         <Image alt="bulb" src='/bridge.webp' height={100} width={180} className="md:absolute w-[20vh]"></Image>
-           <h2 className="md:pt-[15vh] whitespace-pre-wrap text-center text-5xl md:text-8xl font-medium tracking-tighter text-black dark:text-white">
+           <h2 className="md:pt-[15vh] whitespace-pre-wrap text-center text-5xl md:text-8xl lg:text-8xl font-medium tracking-tighter text-black dark:text-white">
              Bridging the Gap Between Education and Industry!
             </h2> 
             <p className="text-sm my-2 md:text-xl tracking-tight leading-[20px]">START YOUR CAREER WITH INTERNPRO !!!</p>
-            <a className="hover:bg-yellow-300 transition duration-200 py-2 px-4 font-semibold rounded-full light:bg-black dark:bg-white light:text-white dark:text-black my-1 text-lg md:text-xl" href="https://docs.google.com/forms/d/e/1FAIpQLSez4WUhG__Biy7ldYhc8Dzke9pqoQ92KVBH--aHxMzTV4mPmA/viewform">Join Now</a>
+            <a className="bg-black dark:bg-white py-2 px-4 font-semibold rounded-full text-white dark:text-black my-1 text-lg md:text-xl" href="https://docs.google.com/forms/d/e/1FAIpQLSez4WUhG__Biy7ldYhc8Dzke9pqoQ92KVBH--aHxMzTV4mPmA/viewform">Join Now</a>
         </div>
       <GridPattern
         width={30}
