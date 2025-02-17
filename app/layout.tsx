6908be87177472fbfc8668df5fe3b11e1b177b5f
Sumit Karanjekar <122661393+thisSumit@users.noreply.github.com>
2025-02-17 20:47:00 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,35 @@
-// import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
-// import "./globals.css";
-// import { ThemeProvider } from "@/components/theme-provider"
-// const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
+const inter = Inter({ subsets: ["latin"] });
 
-// const kd = ['MSEB internship certificate', 'online internship', 'internpro', 'codsoft', 'internpe', 'free internship certificate', 'learn and earn', 'forage', 'virtual internship', 'project based internship'];
+const kd = ['MSEB internship certificate', 'online internship', 'internpro', 'codsoft', 'internpe', 'free internship certificate', 'learn and earn', 'forage', 'virtual internship', 'project based internship'];
 
-// export const metadata: Metadata = {
+export const metadata: Metadata = {
 
-//   title: { default:"InternPro An Internship Platform", 
-//     template: "%s - InternPro",
-//   },
-//   description: "An internship platform, that give MSEB Register Company Intenship Certificate at Major Domain",
-//   keywords: kd,
-// };
+  title: { default:"InternPro An Internship Platform", 
+    template: "%s - InternPro",
+  },
+  description: "An internship platform, that give MSEB Register Company Intenship Certificate at Major Domain",
+  keywords: kd,
+};
 
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en">
-//       <body className={inter.className}><ThemeProvider
-//             attribute="class"
-//             defaultTheme="light"
-//             enableSystem
-//             disableTransitionOnChange
-//           >
-//             {children}
-//           </ThemeProvider></body>
-//     </html>
-//   );
-// }
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}><ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider></body>
+    </html>
+  );
+}
